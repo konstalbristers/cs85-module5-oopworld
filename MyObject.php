0762c8008b50a389dd746db01ae3a459d1360290
konstalbristers <kcparis33@gmail.com>
2025-07-27 21:30:07 -0700

    added WorkoutLog class with properties

---
+++ MyObject.php
@@ -0,0 +1,12 @@
+<?php
+// WorkoutLog.php
+// Assignment: cs85-module5b-oopworld
+
+class WorkoutLog {
+    public $name;
+    public $workoutType;
+    public $duration;
+    public $caloriesBurned;
+    public $notes;
+}
+?>
