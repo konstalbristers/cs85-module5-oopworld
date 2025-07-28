8626ddd14465f7aaa6753547ef6da32048c2cbe3
konstalbristers <kcparis33@gmail.com>
2025-07-27 21:34:58 -0700

    added constructor to initialize properties

--- MyObject.php
+++ MyObject.php
@@ -8,5 +8,14 @@ class WorkoutLog {
     public $duration;
     public $caloriesBurned;
     public $notes;
+
+    public function __construct($name, $workoutType, $duration, $caloriesBurned, $notes) {
+        // i expect this to initialize all properties when object is created
+        $this->name = $name;
+        $this->workoutType = $workoutType;
+        $this->duration = $duration;
+        $this->caloriesBurned = $caloriesBurned;
+        $this->notes = $notes;
+    }
 }
 ?>
